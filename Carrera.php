ef4f6ea4cb86520387cbe9a9e375249b4cd5729e
José Alberto Goncalves <gml.josea2@gmail.com>
2011-05-04 21:04:09 -0430

    Definición de la clase Carrera y sus métodos. Agregué .gitignore
    Definí los métodos pero aún no los he implementado.
    El .gitignore e sun archivo que usa git para determinar que archivos
    ni siquiera va a mostrar en el git status. Los archivos que hagan match
    de alguna línea Git nunca les prestará atención.

---
+++ Carrera.php
@@ -0,0 +1,73 @@
+<?php
+class Carrera {
+  private $nombre;
+  private $direccion;
+  private $coordinador;
+  private $codigo;
+
+  public function __construct() {
+
+  }
+
+  public static function all() {
+
+  }
+
+  public static function getByKey($key) {
+ 
+  }
+  
+  public function save() {
+
+  }
+
+  public function delete() {
+
+  }
+
+  public function toString() {
+
+  }
+
+  public function __construct() {
+
+
+  }
+
+  public function getNombre() {
+
+
+  }
+
+  public function getDireccion() {
+
+  }
+
+  public function getCoordinador() {
+
+
+  }
+
+  public function getCodigo() {
+
+  }
+
+  public function setNombre($nom) {
+
+
+  }
+
+  public function setDireccion($dir) {
+
+  }
+
+  public function setCoordinador($coo) {
+
+
+  }
+
+  public function setCodigo($cod) {
+
+
+  }
+}
