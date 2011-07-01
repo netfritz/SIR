7e2d689ce94cd4b86eb4835b50eb3401f5e5eb20
Victor De Ponte <throoze@gmail.com>
2011-07-01 08:42:23 -0430

    kbbbnsklbnvkjlndfs

--- src/vistas/PerfilView.php
+++ src/vistas/PerfilView.php
@@ -46,7 +46,7 @@ class PerfilView {
   //#####################################################################//
 
   public function viewRegisterEmpty(){
-    return "<form name=\"registro\" action=\"bienvenida.php\" method=\"POST\">
+    return "<form name=\"registro\" action=\"bienvenida.php?Action=create&mode=request\" method=\"POST\">
                       <table>
                         <tbody>
                           <tr>
@@ -103,5 +103,65 @@ class PerfilView {
                     </form>";
   }
   
+  public function viewEditPerfil($perfil,$status) {
+    if (strcmp($status,"edit")) {
+      $msg = "<form name=\"registro\" action=\"bienvenida.php?Action=edit&mode=submit\" method=\"POST\">
+                      <table>
+                        <tbody>
+                          <tr>
+                            <label>Username:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"usrname\" type=\"text\" class=\"field\" value=\"{$perfil['usrname']}\"/>
+                          </tr><br/><br/>
+			              <tr>
+                            <label>Nombre:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"name\" type=\"text\" class=\"field\" value=\"{$perfil['name']}\"/>
+                          </tr><br/><br/>
+                          <tr>
+                            <label>Apellido:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"lastname\" type=\"text\" class=\"field\" value=\"{$perfil['lastname']}\"/>
+                          </tr><br/><br/>
+                          <tr>
+                            <label> E-Mail: </label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"email\" type=\"text\" class=\"field\" value=\"{$perfil['email']}\"/>
+                          </tr><br/><br/>
+                          <tr>
+                            <label> Reescriba su email:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"email2\" type=\"text\" class=\"field\" value=\"{$perfil['email2']}\"/>
+                          </tr><br/><br/>
+                          <tr>
+                            <label>Contraseña:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"passwd\" type=\"password\" class=\"field\" />
+                          </tr><br/><br/>
+                          <tr>
+                            <label>Reescriba su Contraseña:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"passwd2\" type=\"password\" class=\"field\"/>
+                          </tr><br/><br/>
+                          <tr>
+                            <label>Fecha de Nacimiento:</label>
+                          </tr><br/><br/>
+                          <tr>
+                            <input name=\"bdate\" type=\"text\" class=\"field\" value=\"{$perfil['bdate']}\"/>
+                          </tr><br/><br/>
+                          <input type=\"submit\" value=\"Enviar!\">
+                        </tbody>
+                      </table>
+                    </form>";
+    }
+  }
+
 }
 ?>
